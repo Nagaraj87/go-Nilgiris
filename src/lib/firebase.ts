dc8ea88ae5020028b1d183c5408170abc735439b
Kannadhasan Karaikudi <karaikudikannadhasan@gmail.com>
2025-09-07 18:31:03 +0000

    Error: Attempted to call getAdminCredentials() from the server but getAd

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,4 @@
 
-'use client';
 import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, query, where, doc, setDoc, getDoc, deleteDoc, updateDoc, arrayUnion, arrayRemove, writeBatch } from 'firebase/firestore';
 import { format, addDays } from 'date-fns';
