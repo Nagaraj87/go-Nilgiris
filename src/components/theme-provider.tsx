a8ee51344e579e5dfce5a1ebc9945d82dbf47f06
Kannadhasan Karaikudi <karaikudikannadhasan@gmail.com>
2025-09-08 18:45:10 +0000

    This current web page color theme is much good. Actually i liked most. B

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,5 +6,12 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider 
+        {...props}
+        themes={['light', 'dark', 'system', 'forest', 'forest-dark', 'sunset', 'sunset-dark', 'indigo', 'indigo-dark', 'rose', 'rose-dark', 'mocha', 'mocha-dark']}
+    >
+        {children}
+    </NextThemesProvider>
+    )
 }
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, Palette, Monitor } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -11,6 +11,8 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
@@ -26,14 +28,42 @@ export function ThemeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          <Sun className="mr-2 h-4 w-4" />
+          <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          <Moon className="mr-2 h-4 w-4" />
+          <span>Dark</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+         <DropdownMenuItem onClick={() => setTheme("system")}>
+          <Monitor className="mr-2 h-4 w-4" />
+          <span>System</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuLabel>Themes</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+         <DropdownMenuItem onClick={() => setTheme("forest")}>
+          <Palette className="mr-2 h-4 w-4 text-green-500" />
+          <span>Forest</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("sunset")}>
+          <Palette className="mr-2 h-4 w-4 text-orange-500" />
+          <span>Sunset</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("indigo")}>
+          <Palette className="mr-2 h-4 w-4 text-indigo-500" />
+          <span>Indigo</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("rose")}>
+          <Palette className="mr-2 h-4 w-4 text-rose-500" />
+          <span>Rose</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("mocha")}>
+          <Palette className="mr-2 h-4 w-4 text-amber-700" />
+          <span>Mocha</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
